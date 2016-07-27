a5f76a547516b0b0d3003ed5bc2b0cc6e33d432b
Justin Yoo <justin.yoo@aliencube.com>
2016-07-27 15:18:32 +1000

    Add condition test

---
+++ test/CplSharp.Tests/Conditions/ConditionTest.cs
@@ -0,0 +1,24 @@
+﻿using CplSharp.Conditions;
+using CplSharp.Tests.Conditions.Fixtures;
+
+using Xunit;
+
+namespace CplSharp.Tests.Conditions
+{
+    /// <summary>
+    /// This represents the test entity for the <see cref="Condition"/> class.
+    /// </summary>
+    public class ConditionTest : IClassFixture<ConditionFixture>
+    {
+        private readonly Condition _condition;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ConditionTest"/> class.
+        /// </summary>
+        /// <param name="fixture"><see cref="ConditionFixture"/> instance.</param>
+        public ConditionTest(ConditionFixture fixture)
+        {
+            this._condition = fixture.Condition;
+        }
+    }
+}
---
+++ test/CplSharp.Tests/Conditions/Fixtures/ConditionFixture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using CplSharp.Conditions;
+
+namespace CplSharp.Tests.Conditions.Fixtures
+{
+    /// <summary>
+    /// This represents the fixture entity for the <see cref="ConditionTest"/> class.
+    /// </summary>
+    public class ConditionFixture : IDisposable
+    {
+        private bool _disposed;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ConditionFixture"/> class.
+        /// </summary>
+        public ConditionFixture()
+        {
+            this.Condition = new Condition();
+        }
+
+        /// <summary>
+        /// Gets the <see cref="CplSharp.Conditions.Condition"/> instance.
+        /// </summary>
+        public Condition Condition { get; }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (this._disposed)
+            {
+                return;
+            }
+
+            this._disposed = true;
+        }
+    }
+}
